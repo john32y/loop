05dd6691d202723fb72f47c91f8d51a79c7910ea
john32y <john.thelronge@gmail.com>
2023-06-13 15:59:16 -0400

    doitagain.js

---
+++ doitagain.js
@@ -0,0 +1,12 @@
+function arrayLoop() {
+
+    const even = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30];
+
+    for (let i=0; i<even.length; i++) {
+        const result = even[i];
+
+        console.log(result);
+    }
+}
+
+arrayLoop();
---
+++ index.js
@@ -0,0 +1,30 @@
+// function firstLoop(total) {
+    
+//     for (let i = 0; i < total; i++) {
+//         console.log(i);
+//     }
+// }
+
+// function fingerLoop() {
+// const totalFinger =10;
+
+// for(let i = 0; i < totalFinger; i++) {
+//     let sentence = 'I have ' + (i+1) + ' fingers';
+//     console.log(sentence);
+// }
+// }
+
+// function arrayLoop() {
+
+//     const prime = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47];
+
+//     for (let i=0; i<prime.length; i++) {
+//         const result = prime[i];
+
+//         console.log(result);
+//     }
+// }
+
+// arrayLoop();
+
+
